4cb3bc8fb70962e05ab6b17bd10d1dd1db75bad0
Mario21-cmd <61766930+Mario21-cmd@users.noreply.github.com>
2020-03-04 06:18:14 -0600

    Add files via upload

---
+++ Tipos de datos.py
@@ -0,0 +1,18 @@
+print('*****************')
+print('name:{}'.format('Charile'))
+print('last_name:{}'.format('Brown'))
+print('Age:{}'.format('18'))
+print('height:{}'.format('1.5 m'))
+print('Drives_license:{}'.format('False'))
+print('study:{}'.format('True'))
+print('******************')
+name='Mario'
+
+
+
+
+
+
+
+
+
